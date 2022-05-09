cbe0b6819acf8e05c990df476add358f8fac50b6
aarongerbrandt <74976900+aarongerbrandt@users.noreply.github.com>
2022-05-08 21:47:32 -0500

    Correct Version

--- validate_form.php
+++ validate_form.php
@@ -1,24 +1,64 @@
 <?php
-    echo ("ur mom");
-    function validate_form($input) {
-        $input = trim($input);
-        $input = trimslashes($input);
-        $input = htmlspecialchars($input);
-        echo ($input);
+    function format_input($input) {
+        $input = trim($input); //remove leading and trailing whitespace
+        $input = htmlspecialchars($input); //formats special characters, prevent xss
         return $input;
     }
 
-    if (isset($_POST['form_submit'])) {
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $gender = $_POST['gender'];
-        $city = $_POST['city'];
-        $state = $_POST['state'];
-        $qualifications = $_POST['qualifications'];
-        $password = $_POST['password'];
+    if (isset($_POST['fname'])){
+        echo (var_dump($_POST));
+        $fname = format_input($_POST['fname']);
+        $lname = format_input($_POST['lname']);
+        $email = format_input($_POST['email']);
+        $phone = format_input($_POST['phone']);
+        $gender = format_input($_POST['gender']);
+        $city = format_input($_POST['city']);
+        $state = format_input($_POST['state']);
+        $qualifications = format_input($_POST['qualifications']);
+        $password = format_input($_POST['password']);
+
+        if(!$fname or preg_match("/^[a-zA-Z'\s]{1,30}$/", $fname) < 1){
+            echo "error with fname";
+            return;
+        }
+        if(!$lname or preg_match("/^[a-zA-Z'\s]{1,30}$/", $lname) < 1){
+            echo "error with lname";
+            echo $lname;
+            return;
+        }
+        if(!$email or preg_match("/[a-z0-9\-\_\.\']+@[a-z0-9]+\.com|edu/", $email) < 1){
+            echo "error with email";
+            return;
+        }
+        if(!$phone or strlen($phone) != 10){
+            echo "error with phone";
+            return;
+        }
+        if(!$gender){
+            echo "error with gender";
+            return;
+        }
+        if(!$city){
+            echo "error with city";
+            return;
+        }
+        if(!$state){
+            echo "error with state";
+            return;
+        }
+        if(!$qualifications){
+            echo "error with qualifications";
+            return;
+        }
+        if(!$password or preg_match("/\A(?=[^a-z]*[a-z])(?=(?:[^A-Z]*[A-Z]))(?=\D*\d).*/", $password) < 1){
+            echo "error with password\n";
+            return;
+        }
+        echo "Valid Input";
     }
-    echo ("ur mom");
+    else{
+        echo "No input recieved. Try again";
+        return;
+    }
 ?>
 
